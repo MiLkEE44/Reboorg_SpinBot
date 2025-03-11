777c21640fd6b559182d24613a27658f7f8be7d5
Michael Bolden <milke44@Michaels-Air.fios-router.home>
2025-03-11 03:47:59 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# Will make Reeborg's World robot spin around,
+# hugging and moving along walls.
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+def hop():
+    if wall_in_front() == True:
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+    else:
+        move()
+
+def big_wall():
+    if wall_in_front() == True:
+        turn_left()
+    else:
+        move()
+
+def down():
+    while wall_in_front() == False:
+        if at_goal() == True:
+            done()
+        else:
+            move()
+            turn_right()
+
+def walk():
+    while front_is_clear():
+        move()
+
+number_of_hurdles = 100
+for hurdles in range(number_of_hurdles):
+    if at_goal() == True:
+        done()
+    elif not at_goal():
+        walk()
+        big_wall()
+        down()
